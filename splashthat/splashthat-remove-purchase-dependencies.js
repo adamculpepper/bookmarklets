6609e2c697e20b23b5b0523840634f9ea7bc5b5a
Adam Culpepper <adam@trendsic.com>
2018-04-19 12:34:06 -0500

    swapping out the image upload path of the troll face image to it's on a secure domain, for the cases where the splashthat site is HTTPS and throws an error of insecure assets

--- splashthat/splashthat-remove-purchase-dependencies.js
+++ splashthat/splashthat-remove-purchase-dependencies.js
@@ -2,6 +2,6 @@ $('[data-parent-ticket-type-ids]')
     .attr('data-parent-ticket-type-ids', '')
     .removeClass('hide')
     .css({'background': 'lightblue'})
-    .before('<span style="position:absolute; top:-10px; left:-12px; z-index:3;"><img src="http://swagress.com/assets/img/troll-face-left-small.png" width="35"></span>')
+    .before('<span style="position:absolute; top:-10px; left:-12px; z-index:3;"><img src="https://raw.githubusercontent.com/adamculpepper/bookmarklets/master/assets/img/troll-face-left-small.png" width="35"></span>')
     .parent()
     .css({'position': 'relative'});
