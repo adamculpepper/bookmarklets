f679a5a9b3a89d47049db77e26defbf07e235613
Adam Culpepper <adam@trendsic.com>
2018-04-20 09:08:28 -0500

    removing the "cdn" subdomain from the troll image for consistency.

--- splashthat/splashthat-remove-purchase-dependencies.js
+++ splashthat/splashthat-remove-purchase-dependencies.js
@@ -2,6 +2,6 @@ $('[data-parent-ticket-type-ids]')
     .attr('data-parent-ticket-type-ids', '')
     .removeClass('hide')
     .css({'background': 'lightblue'})
-    .before('<span style="position:absolute; top:-10px; left:-12px; z-index:3;"><img src="https://cdn.rawgit.com/adamculpepper/bookmarklets/master/assets/img/troll-face-left-small.png" width="35"></span>')
+    .before('<span style="position:absolute; top:-10px; left:-12px; z-index:3;"><img src="https://rawgit.com/adamculpepper/bookmarklets/master/assets/img/troll-face-left-small.png" width="35"></span>')
     .parent()
     .css({'position': 'relative'});
